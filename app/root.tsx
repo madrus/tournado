4765442573ca255126b1c436e1a5e7724360a0fa
Andre Roussakoff <madrus@gmail.com>
2025-04-15 09:23:55 +0200

    fix: add alias to vite.config.ts

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
   ScrollRestoration,
 } from '@remix-run/react'
 
-import tailwindStyles from '@/styles/tailwind.css'
+import tailwindStyles from '@/styles/tailwind.css?url'
 import { getUser } from '@/utils/session.server'
 
 export const links: LinksFunction = () => [
